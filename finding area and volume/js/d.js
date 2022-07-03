3322456ef109d91941f4b7d61045f499200afccc
Varin Saipanya <94356640+Varin471@users.noreply.github.com>
2022-07-03 08:03:15 +0700

    Update d.js

--- finding area and volume/js/d.js
+++ finding area and volume/js/d.js
@@ -1,10 +1,9 @@
 // สูตรการหาพื้นที่สี่เหลี่ยมขนมเปียกปูน
 // RUNFILE: node d.js
 
-const promptsync = require('prompt-sync')();
-var use = promptsync("การหาพื้นที่สี่เหลี่ยมขนมเปียกปูน \n 1.) พิมพ์ตัวเลือกที่ (1) เพื่อใช้สูตร ฐาน x สูง\n 2.) พิมพ์ตัวเลือกที่ (2) เพื่อใช้สูตร 1/2 x ผลคูณของเส้นทแยงมุม\n")
-var error = "ผิดพลาด"
-
+const input = require('prompt-sync')()
+console.log("การหาพื้นที่สี่เหลี่ยมขนมเปียกปูน \n 1.) พิมพ์ตัวเลือกที่ (1) เพื่อใช้สูตร ฐาน x สูง\n 2.) พิมพ์ตัวเลือกที่ (2) เพื่อใช้สูตร 1/2 x ผลคูณของเส้นทแยงมุม\n")
+let use = input("= ")
 switch (use) {
     case "1":
         base = promptsync("ฐาน : ") // ฐาน
@@ -17,5 +16,6 @@ switch (use) {
         console.log(1 / 2 * The_product_of_the_diagonal)
         break
     default:
+        let error = "เกิดข้อผิดพลาดขึ้น!!!"
         console.log(error)
 }
