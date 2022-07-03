5622041f2cc94e6a9b6a0bea5f8d253d5a0cd438
Varin Saipanya <94356640+Varin471@users.noreply.github.com>
2022-07-03 08:11:16 +0700

    Update g.js

--- finding area and volume/js/g.js
+++ finding area and volume/js/g.js
@@ -1,15 +1,14 @@
 // สูตรการหาพื้นที่สี่เหลี่ยมด้านไม่เท่า
 // RUNFILE: node g.js
 
-const promptsync = require('prompt-sync')()
-const cal = function(diagonal, The_sum_of_the_branches, radius, radius2) {
+const input = require('prompt-sync')()
+const calculate = function(diagonal, The_sum_of_the_branches, radius, radius2) {
     console.log("การหาพื้นที่สี่เหลี่ยมด้านไม่เท่า")
-    radius = promptsync("รัศมี = ") // รัศมี
+    radius = input("รัศมี = ") // รัศมี
     radius2 = Math.pow(radius, 2) // รัศมีกำลัง2
     The_sum_of_the_branches = Math.PI * (radius2) // ผลบวกของเส้นกิ่ง
-    diagonal = promptsync("เส้นทแยงมุม = ") // เส้นทแยงมุม 
+    diagonal = input("เส้นทแยงมุม = ") // เส้นทแยงมุม 
     let result = 1 / 2 * diagonal * The_sum_of_the_branches // ปัดค่าขึ้น / ลง
-    console.log(Math.round(result))
+    console.log(Number(Math.round(result)))
 }
-
-cal()
+calculate ()
