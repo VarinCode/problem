265e24824dfb71750487303bece089df3e9c9851
Varin Saipanya <94356640+Varin471@users.noreply.github.com>
2022-07-03 08:16:37 +0700

    Update n.js

--- finding area and volume/js/n.js
+++ finding area and volume/js/n.js
@@ -1,11 +1,11 @@
 // สูตรการหาปริมาตรปริซึม
 // RUNFILE: node n.js
 
-const promptsync = require('prompt-sync')()
+const input = require('prompt-sync')()
 console.log("การหาปริมาตรปริซึม")
-function cal(base_area, high) {
-    base_area = promptsync("พื้นที่ฐาน = ") // พื้นที่ฐาน
-    high = promptsync("สูง = ") // สุง
+function calculate(base_area, high) {
+    base_area = input("พื้นที่ฐาน = ") // พื้นที่ฐาน
+    high = input("สูง = ") // สุง
     return base_area * high
 }
-console.log(cal())
+console.log(calculate())
