06c9d172d5c556b34ef0d13efa0ceb2c5028e753
Varin Saipanya <94356640+Varin471@users.noreply.github.com>
2022-07-03 08:14:16 +0700

    Update l.js

--- finding area and volume/js/l.js
+++ finding area and volume/js/l.js
@@ -1,9 +1,9 @@
 // สูตรการหาปริมาตรทรงกระบอก
 // RUNFILE: node l.js
 
-const promptsync = require('prompt-sync')()
+const input = require('prompt-sync')()
 console.log("การหาปริมาตรทรงกระบอก")
 let PI = 22 / 7 // พาย
-let radius = promptsync("รํศมี = ") ** 2 // รัศมีกำลัง2
-let high = promptsync("สูง = ") // สูง
+let radius = input("รํศมี = ") ** 2 // รัศมีกำลัง2
+let high = input("สูง = ") // สูง
 console.log(Math.round(PI * radius * high))
