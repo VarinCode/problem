62c5225a94e31af24f4e01b4a7544a32df573e4a
Varin Saipanya <94356640+Varin471@users.noreply.github.com>
2022-07-03 08:15:32 +0700

    Update m.js

--- finding area and volume/js/m.js
+++ finding area and volume/js/m.js
@@ -1,15 +1,15 @@
 // สูตรการหาปริมาตรทรงกรวย 
 // RUNFILE: node m.js
 
-const promptsync = require('prompt-sync')()
+const input = require('prompt-sync')()
 console.log("การหาปริมาตรทรงกรวย ")
-var radius // รัศมี
-var high // สูง
-cal = (radius2, result) => {
-    radius = promptsync("รัศมี = ")
-    high = promptsync("สูง = ")
+let radius // รัศมี
+let high // สูง
+const calculate = (radius2, result) => {
+    radius = input("รัศมี = ")
+    high = input("สูง = ")
     radius2 = Math.pow(radius, 2)
     result = 1 / 3 * Math.PI * (radius2 * high)
     return Math.round(result)
 }
-console.log(cal())
+console.log(calculate())
