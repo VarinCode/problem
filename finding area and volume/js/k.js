2bfb9ce426b15697596057ead6fae398dc1cb251
Varin Saipanya <94356640+Varin471@users.noreply.github.com>
2022-07-03 08:13:26 +0700

    Update k.js

--- finding area and volume/js/k.js
+++ finding area and volume/js/k.js
@@ -1,12 +1,12 @@
 // สูตรการหาปริมาตรทรงกลม
 // RUNFILE: node k.js
 
-const promptsync = require('prompt-sync')()
+const input = require('prompt-sync')()
 console.log("การหาปริมาตรทรงกลม")
-cal = (radius, result, pi) => {
-    radius = promptsync("รัศมี = ") // รัศมี
+const calculate = (radius, result, pi) => {
+    radius = input("รัศมี = ") // รัศมี
     pi = Math.PI * (radius ** 3) // พาย
     result = 4 / 3 * pi
     return Math.round(result)
 }
-console.log(cal())
+console.log(calculate())
