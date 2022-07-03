2b0da7b879fcdee322d028c8d0a774284d79c9e4
Varin Saipanya <94356640+Varin471@users.noreply.github.com>
2022-07-03 08:09:39 +0700

    Update f.js

--- finding area and volume/js/f.js
+++ finding area and volume/js/f.js
@@ -1,14 +1,12 @@
 // สูตรการหาพื้นที่สี่เหลี่ยมรูปว่าว
 // RUNFILE: node f.js
 
-const promptsync = require('prompt-sync')()
+const input = require('prompt-sync')()
 const kite_figure = {
     log: console.log("การหาพื้นที่สี่เหลี่ยมรูปว่าว"),
-    cal: function(The_product_of_the_diagonals) {
-
-        value = promptsync(The_product_of_the_diagonals + " = ") // ผลคูณของเส้นทแยงมุม
+    cal: function(The_product_of_the_diagonals, value) {
+        value = input(The_product_of_the_diagonals + " = ") // ผลคูณของเส้นทแยงมุม
         return 1 / 2 * value
     }
 }
-
 console.log(kite_figure.cal("ผลคูณของเส้นทแยงมุม"))
