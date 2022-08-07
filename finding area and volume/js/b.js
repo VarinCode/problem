04a07807ebdb169a05879edbbf6fb08b3e5159a8
VarinCode <94356640+VarinCode@users.noreply.github.com>
2022-08-07 20:12:10 +0700

    Delete finding area and volume directory

--- finding area and volume/js/b.js
+++
@@ -1,9 +0,0 @@
-// สูตรการหาพื้นที่สี่เหลี่ยมผืนผ้า
-// RUNFILE: node b.js
-
-const input = require('prompt-sync')()
-console.log("การหาพื้นที่สี่เหลี่ยมผืนผ้า")
-let wide = input("กว้าง : ") // กว้าง
-let long = input("ยาว : ") // ยาว
-let result = wide * long
-console.log(result)
--- finding area and volume/js/c.js
+++
@@ -1,9 +0,0 @@
-// สูตรการหาพื้นที่สามเหลี่ยม
-// RUNFILE: node c.js
-
-const input = require('prompt-sync')()
-console.log("การหาพื้นที่สามเหลี่ยม")
-let base = input("ฐาน :") // ฐาน 
-let height = input("สูง : ") // สูง
-let result = 1 / 2 * base * height
-console.log(result)
